93a445196ea4ce37f0e3ae63f4e7f8f478a1494b
Ryba218 <vijock@gmail.com>
2020-10-09 18:54:18 +0200

    DbService

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,6 +1,9 @@
 package com.crud.tasks.controller;
 
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,14 +14,20 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
+
         return new ArrayList<>();
     }
 
     @RequestMapping(method =RequestMethod.GET, value = "getTask")
     public TaskDto getTask(Long taskId) {
+
         return new TaskDto(1L,"test title", "test_content");
     }
 
@@ -29,6 +38,7 @@ public class TaskController {
 
     @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
     public TaskDto updateTask (TaskDto taskDto) {
+
         return new TaskDto(1L,"Edited test title", "Test content");
     }
 
