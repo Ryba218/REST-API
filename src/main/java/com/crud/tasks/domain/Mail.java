9bbeac734e796761df13c4130e39cff9ba042ecf
Ryba218 <vijock@gmail.com>
2020-12-12 19:19:09 +0100

    Mail

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,17 @@
+package com.crud.tasks.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.aspectj.lang.annotation.Aspect;
+
+@Getter
+@AllArgsConstructor
+public class Mail {
+    private String mailTo;
+    private String subject;
+    private String message;
+    private String toCC;
+
+
+}
